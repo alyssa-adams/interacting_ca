3894545e90fb400b3560267063265bd3b3c3d839
alyssa-adams <amadam15@asu.edu>
2020-05-17 23:47:00 -0500

    88 rules instead of 256

--- analyze_results.py
+++ analyze_results.py
@@ -3,11 +3,21 @@ import pickle
 from run_model import RegularCA
 from pybdm import BDM
 from pybdm.partitions import PartitionRecursive
+import numpy as np
 
 
 wmax = 3
-bdm = BDM(ndim=1)
-bdm_traj = BDM(ndim=1, nsymbols=8, warn_if_missing_ctm=False, partition=PartitionRecursive)
+bdm = BDM(ndim=1, warn_if_missing_ctm=False, partition=PartitionRecursive)
+bdm_traj = BDM(ndim=1, nsymbols=9, warn_if_missing_ctm=False, partition=PartitionRecursive)
+
+# map states to integers
+ks = ['111', '110', '101', '100', '011', '010', '001', '000']
+vs = list(range(8))
+states_ints_map = dict(zip(ks, vs))
+
+# only use the 88 unique ca rules for this analysis, since it's taking a while to come up with results
+# (will reduce redundancy anyways)
+use_these_rules = [0, 1, 4, 5, 18, 19, 22, 23, 32, 33, 36, 37, 50, 51, 54, 55, 72, 73, 76, 77, 90, 91, 94, 95, 104, 105, 108, 109, 122, 123, 126, 127, 128, 129, 132, 133, 146, 147, 150, 151, 160, 161, 164, 165, 178, 179, 182, 183, 200, 201, 204, 205, 218, 219, 222, 223, 232, 233, 236, 237, 250, 251, 254, 255]
 
 # ============ ECA trajectories ==============
 
@@ -30,12 +40,21 @@ rule_types = ['both_states', 'this_state', 'other_state', 'mixed']
 # load the pickle files
 for rule_type in rule_types:
 
-    with open(rule_type + '_results.csv', 'w+') as csvfile:
+    print(rule_type)
 
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['rule_i', 'ca1_r', 'ca2_r', 'ca1_is', 'ca2_is'])
+    files = list(filter(lambda x: re.search('\d', x), os.listdir(os.path.join('trajectories', rule_type))))
+    files = list(filter(lambda x: int(x.split('_')[3]) in use_these_rules and int(x.split('_')[4]) in use_these_rules, files))
 
-        for file in list(filter(lambda x: re.search('\d', x), os.listdir(os.path.join('trajectories', rule_type)))):
+    for file in files:
+
+        with open(os.path.join('measurements', rule_type, file + '_results.csv'), 'w+') as csvfile:
+
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['ca1_inn', 'ca2_inn', 'tr',
+                                'ca1_bdm_max', 'ca2_bdm_max', 'ca1_bdm_mean', 'ca2_bdm_mean', 'ca1_bdm_mean_last',
+                                'ca2_bdm_mean_last', 'ca1_bdm_traj', 'ca2_bdm_traj'])
+
+            print(file)
 
             # load in file
             data = pickle.load(open(os.path.join('trajectories', rule_type, file), "rb"))
@@ -76,8 +95,8 @@ for rule_type in rule_types:
                         tr = sr_rep[1]  # this means it repeats after THIS many time steps
 
                 # get bdm for each state
-                ca1_bdm = [bdm.bdm(s, normalized=True) for s in ca1_state_traj]
-                ca2_bdm = [bdm.bdm(s, normalized=True) for s in ca2_state_traj]
+                ca1_bdm = [bdm.bdm(np.array(list(s), dtype=int), normalized=True) for s in ca1_state_traj]
+                ca2_bdm = [bdm.bdm(np.array(list(s), dtype=int), normalized=True) for s in ca2_state_traj]
 
                 # max, mean, last few states mean
                 ca1_bdm_max = max(ca1_bdm)
@@ -89,8 +108,10 @@ for rule_type in rule_types:
 
                 # get bdm for the whole trajectory
                 # 2^w states means 2^3 = 8 states, can use one state per symbol here
-                ca1_bdm_traj = bdm_traj.bdm(ca1_state_traj, normalized=True)
-                ca2_bdm_traj = bdm_traj.bdm(ca2_state_traj, normalized=True)
+                ca1_bdm_traj = bdm_traj.bdm(np.array([states_ints_map[s] for s in ca1_state_traj]), normalized=True)
+                ca2_bdm_traj = bdm_traj.bdm(np.array([states_ints_map[s] for s in ca2_state_traj]), normalized=True)
 
                 # save to file
-                ca1_bdm
+                data_row = [ca1_inn, ca2_inn, tr, ca1_bdm_max, ca2_bdm_max, ca1_bdm_mean, ca2_bdm_mean, ca1_bdm_mean_last, ca2_bdm_mean_last, ca1_bdm_traj, ca2_bdm_traj]
+
+                csvwriter.writerow(data_row)
